cf52f82c930f0982b82e9ae4841233f0ea9ac691
Mriganko <mrigankodey35@gmail.com>
2026-08-28 23:30:16 +0530

    Change model to groq

--- backend/ai_api.py
+++ backend/ai_api.py
@@ -1,17 +1,19 @@
-import google.genai as genai
+from groq import Groq
+from dotenv import load_dotenv
+import os
 
-API_KEY = "AQ.Ab8RN6Luna606R0NgPAl9p_b7mohN2Dx3hhvTQ7ZyoPvWtKqlg"
-client = genai.Client(api_key=API_KEY)
+load_dotenv()
+client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
 
 def ask(ques,weather_data):
-    weather_text = {
+    weather_text = (
         f"City: {weather_data['city']}\n"
         f"Temperature: {weather_data['temperature']}°C\n"
         f"Feels like: {weather_data['feels_like']}°C\n"
         f"Humidity: {weather_data['humidity']}%\n"
         f"Conditions: {weather_data['description']}\n"
         f"Wind speed: {weather_data['wind_speed']} m/s"
-    }
+    )
 
     prompt = f"""You are a helpful weather assistant for farmers and everyday people in India.
  
@@ -24,16 +26,25 @@ Answer clearly and simply in 2-3 sentences, in plain language a non-expert would
 If relevant, mention any practical advice (e.g. carrying an umbrella, safety during heavy rain, etc).
 """
 
-    response = client.models.generate_content(
-        model="gemini-3.6-flash",
-        contents=prompt
-    )
+    response = client.chat.completions.create(
+    model="openai/gpt-oss-20b",
+    messages=[
+        {
+            "role": "system",
+            "content": "You are WeatherGPT, a helpful weather assistant."
+        },
+        {
+            "role": "user",
+            "content": prompt
+        }
+    ]
+)
 
-    return response.text
+    return response.choices[0].message.content
 
 if __name__ == "__main__":
     # Fake sample weather data (remove the docstrings to use)
-    '''
+    
     sample_weather = {
         "city": "Kolkata",
         "temperature": 28.97,
@@ -41,7 +52,7 @@ if __name__ == "__main__":
         "humidity": 89,
         "description": "moderate rain",
         "wind_speed": 0
-    }'''
+    }
     question = input("Ask a weather question: ")
     answer = ask(question, sample_weather)
  
